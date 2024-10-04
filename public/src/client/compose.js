f9539ce50493b4900aa15dd990a3415953dfd5b9
Maryam Alkhater <maryamalkhater@maryams-mbp.wifi.local.cmu.edu>
2024-10-04 16:55:42 -0400

    created a handler for anonymous post checkbox

--- public/src/client/compose.js
+++ public/src/client/compose.js
@@ -1,18 +1,42 @@
-'use strict';
-
-
 define('forum/compose', ['hooks'], function (hooks) {
-	const Compose = {};
+    const Compose = {};
 
-	Compose.init = function () {
-		const container = $('.composer');
+    Compose.init = function () {
+        const container = $('.composer');
 
-		if (container.length) {
-			hooks.fire('action:composer.enhance', {
-				container: container,
-			});
-		}
-	};
+        if (container.length) {
+            hooks.fire('action:composer.enhance', {
+                container: container,
+            });
+        }
 
-	return Compose;
+        handleAnonymousPost();
+    };
+
+    return Compose;
 });
+
+function handleAnonymousPost() {
+	console.log('handleAnonymousPost function called');
+    const $checkbox = $('[component="composer"] .form-check-input');
+
+    $checkbox.on('click', async function (event) {
+        const isChecked = $checkbox.is(':checkd');
+        const pid = ajaxify.data.pid;
+
+        try {
+            const response = await api.post(`/posts/${pid}/anonymous`, {
+                anonymous: isChecked,
+            });
+
+            if (response.success) {
+                console.log('Anonymous post status updated successfully.');
+            } else {
+                console.error('Failed to update anonymous post status.');
+            }
+        } catch (error) {
+            console.error('Error updating anonymous post status: ', error);
+        }
+    });
+}
+
