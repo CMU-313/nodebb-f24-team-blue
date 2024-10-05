596a630c03efb4d585a53a8bcca5908ecc2fa400
Bchenjamin <bzchen@andrew.cmu.edu>
2024-10-05 15:11:38 -0400

    Remove anonymous field for post data and added it to topic data

--- src/topics/create.js
+++ src/topics/create.js
@@ -33,6 +33,7 @@ module.exports = function (Topics) {
 			lastposttime: 0,
 			postcount: 0,
 			viewcount: 0,
+			anonymous: data.anonymous,
 		};
 
 		if (Array.isArray(data.tags) && data.tags.length) {
@@ -123,7 +124,7 @@ module.exports = function (Topics) {
 		postData.tid = tid;
 		postData.ip = data.req ? data.req.ip : null;
 		postData.isMain = true;
-		postData.anonymous = data.anonymous || false; // Including anonymous field in post data
+
 		postData = await posts.create(postData);
 		postData = await onNewPost(postData, data);
 
