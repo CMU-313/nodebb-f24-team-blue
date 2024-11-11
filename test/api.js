c9026f6bfde18b8564ed669bc1264f93446553e6
Bchenjamin <bzchen@andrew.cmu.edu>
2024-11-11 13:22:21 -0500

    Fixed linter

--- test/api.js
+++ test/api.js
@@ -664,10 +664,10 @@ describe('API', async () => {
 			if (additionalProperties) { // All bets are off
 				return;
 			}
-			
+
 			// Skip tests for isEnglish and translatedContent (they break tests)
 			if (prop === 'isEnglish' || prop === 'translatedContent') {
-				return; 
+				return;
 			}
 
 			assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context})`);
